5850ff999dff5c5dac06dbce3c9dfd10fdc075b7
Maftuh Zaidi <maftuhzaidi93@gmail.com>
2022-01-10 09:47:29 +0700

    commit

--- application/controllers/functions/Medrec_func.php
+++ application/controllers/functions/Medrec_func.php
@@ -38,4 +38,22 @@ class Medrec_func extends CI_Controller
         echo json_encode($data);
     }
 
+    function save_mr_borrow()
+    {
+        $mr = $this->input->post('mr');
+        $get = $this->mr->getMedrec($mr);
+
+        $result = array(
+            'MR' => $get->MR,
+            'NAMA' => $get->NAMA,
+            'TEMPAT_LAHIR' => $get->TEMPAT_LAHIR,
+            'TGL_LAHIR' => $get->TGL_LAHIR,
+            'ALAMAT' => $get->ALAMAT
+        );
+
+        $data = $result;
+        echo json_encode($data);
+    }
+
+
 }
