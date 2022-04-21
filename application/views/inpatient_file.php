bb9d111c5c7fabd4cbd0c89df75589e5f615a94d
Mafzaidi <maftuhzaidi93@gmail.com>
2022-04-22 04:41:10 +0700

    detail inp file part-2

--- application/views/inpatient_file.php
+++ application/views/inpatient_file.php
@@ -41,31 +41,65 @@
                 </div> -->
             <!-- </div> -->
             <div class="row">  
-                <div class="col-md-4">
+                <div class="col-md-8">
                     <div class="card bg-transparent border rounded-3">
                         <div class="card-header bg-transparent border-bottom">
                             <h5 class="card-header-title mb-0">Data Pasien</h5>
                         </div>
                         <div class="card-body py-0">
-                            <div class="tb">
-                                <div class="tb-body">
-                                    <div class="row tb-row">
-                                        <div class="col-sm-12 col-md-6 tb-cell p-0 font-weight-bolder">Medrec</div>
-                                        <div class="col-sm-12 col-md-6 tb-cell p-0">000000</div>                                     
-                                    </div>
-                                    <div class="row tb-row">
-                                        <div class="col-sm-12 col-md-12 tb-cell p-0">Nama Pasien</div> 
-                                        <div class="col-sm-12 col-md-12 tb-cell p-0">Maftuh Zaidi</div>
-                                    </div>
-                                    <div class="row tb-row"> 
-                                    </div>
+                            <div class="row py-1">
+                                <div class="col-sm-12 col-md-6 p-0">
+                                    <ul class="list-group">
+                                        <li class="list-group-item border-0 py-1">
+                                            <spanc class="h6 text-muted">No. Medrec</spanc>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6 text-muted">Nama Pasien</span>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6 text-muted">Ruangan</span>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6 text-muted">NS</span>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6 text-muted">DPJP</span>
+                                        </li>
+                                    </ul>
                                 </div>
+                                <div class="col-sm-12 col-md-6 p-0">
+                                    <ul class="list-group">
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6">000000</span>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6">Maftuh Zaidi</span>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6">3021</span>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6">Cempaka</span>
+                                        </li>
+                                        <li class="list-group-item border-0 py-1">
+                                            <span class="h6">Petrus, Dr</span>
+                                        </li>
+                                    </ul>
+                                </div>                                     
+                            </div>
+                        </div>
+                        <div class="dropdown-divider"></div>
+                        <div class="row">
+                            <div class="card-body">
+                                <label  class="h6 text-muted col-sm-12 col-md-12 col-lg-6">Rekanan Pasien</label>
+                                <span class="h6 col-sm-12 col-md-12 col-lg-6">-RSMK PASIEN UMUM</span>
+                                <label  class="h6 text-muted col-sm-12 col-md-12 col-lg-6">Tanggal Masuk</label>
+                                <span class="h6 col-sm-12 col-md-12 col-lg-6">10.10.2022</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                   
+                <div class="col-md-4">                  
                 </div>
             </div>
         </div>
